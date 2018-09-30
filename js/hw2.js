52407b3d625452a40e45cff6d0643d21067f1be3
body773 <body.nv@gmail.com>
2018-09-30 23:26:51 +0300

    fff

--- js/hw2.js
+++ js/hw2.js
@@ -1,3 +1,3 @@
- jQuery.get('hw2task4.py', function(data) {
-    document.getElementById("jq").innerHTML = data;
+ jQuery.get('hw/2/hw2task4.py', function(data) {
+    document.getElementById("hw2").innerHTML = data;
 },'Text');
