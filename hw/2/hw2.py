bce22c2d980c81cd5294cbedb1e24548fbbbcee2
body773 <body.nv@gmail.com>
2018-10-02 10:04:42 +0300

    update task2

--- hw/2/hw2.py
+++ hw/2/hw2.py
@@ -1,13 +1,23 @@
 
-def numbers():
+def changenum ():
 	mylist = []
-	for i in range(1,30):
-		if i % 3 == 0:
-			mylist.append("Fizz")
-		elif i % 5 == 0:
-			mylist.append("Bizz")
+	for i in range(1,31):
+		b = list(map(int,str(i)))
+		if len(list(b))==1:
+			if i%3==0:
+				mylist.append("Buzz")
+			elif i%5==0:
+				mylist.append("Fuzz")
+			else:
+				mylist.append(i)
 		else:
-			mylist.append(i)
+			if i%3==0 and i%5==0:
+				mylist.append("BuzzFuzz")
+			elif i%3==0:
+				mylist.append("Buzz")
+			elif i%5==0:
+				mylist.append("Fuzz")
+			else:
+				mylist.append(i)
 	return mylist
-print(numbers())
-
+print(changenum ())
