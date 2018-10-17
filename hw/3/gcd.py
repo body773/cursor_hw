0e75002198b443166275191fc4e3dec7bffeb4b3
body773 <body.nv@gmail.com>
2018-10-17 16:28:21 +0300

    correct py file

--- hw/3/gcd.py
+++ hw/3/gcd.py
@@ -1,5 +1,3 @@
-# Wrong gcd find 5 mistakes
-
 def gcd(a, b):
     assert a >= 0 and b >= 0
     while a and b:
@@ -19,8 +17,3 @@ print ('result: ',gcd(10, 0))
 print ('result: ',gcd(123, 3))
 print ('result: ',gcd(10000000, 64))
 print ('result: ',gcd(0, 0))
-# Examples
-#gcd(10, 0) #=> 10
-#gcd(123, 3) #=> 3
-#gcd(1000000, 64) #=> 64
-#gcd(0, 0) #=> 0
